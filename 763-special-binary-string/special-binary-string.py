715f6f60478c4a6a9ba3c7945b28f61ce2d0d693
shashank <138709484+shxshankgupta@users.noreply.github.com>
2026-02-20 21:49:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (45.61%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+        count = 0
+        i = 0
+        res = []
+        
+        for j, char in enumerate(s):
+            count += 1 if char == '1' else -1
+            if count == 0:
+                res.append("1" + self.makeLargestSpecial(s[i + 1 : j]) + "0")
+                i = j + 1
+        
+        
+        res.sort(reverse=True)
+        return "".join(res)

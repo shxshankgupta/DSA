5527ef3c16eeb7866168e0b3a21175e36da0280d
shashank <138709484+shxshankgupta@users.noreply.github.com>
2026-02-20 19:18:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (43.54%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,4 +1,17 @@
-import math
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return math.pow(x,n)
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        res = 1.0
+        current_product = x
+        
+        while n > 0:
+            if n % 2 == 1:
+                res *= current_product
+            
+            current_product *= current_product
+            n //= 2  
+            
+        return res

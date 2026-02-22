26a988f1a34294f6cce1ded3b2e00854dc03ee9d
shashank <138709484+shxshankgupta@users.noreply.github.com>
2026-02-22 12:30:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (55.21%) - LeetSync

--- 899-binary-gap/binary-gap.py
+++ 899-binary-gap/binary-gap.py
@@ -1,12 +1,12 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
-        binary = bin(n)[2:] # Convert to '10110'
+        binary = bin(n)[2:] 
         last_idx = -1
-        res = 0
+        gap = 0
         
         for i, bit in enumerate(binary):
             if bit == '1':
                 if last_idx != -1:
-                    res = max(res, i - last_idx)
+                    gap = max(gap, i - last_idx)
                 last_idx = i
-        return res
+        return gap
